514265d48c24e7e4eb4f9ac7e7dd5ad6a98dcef0
Florian <florian.zechmeister@digitalservice.bund.de>
2024-10-29 23:52:03 +0100

    Fix(test): Mock subscriber client

--- src/test/java/de/bund/digitalservice/a2j/integration/ReceiverIntegrationTest.java
+++ src/test/java/de/bund/digitalservice/a2j/integration/ReceiverIntegrationTest.java
@@ -3,12 +3,14 @@ package de.bund.digitalservice.a2j.integration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import dev.fitko.fitconnect.client.SubscriberClient;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -16,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @Tag("integration")
 public class ReceiverIntegrationTest {
   @Autowired private MockMvc mockMvc;
+  @MockBean SubscriberClient subscriberClient;
 
   @Test
   void shouldSecureCallbackWithoutAuthorization() throws Exception {
--- src/test/java/de/bund/digitalservice/a2j/integration/SecurityTxtIntegrationTest.java
+++ src/test/java/de/bund/digitalservice/a2j/integration/SecurityTxtIntegrationTest.java
@@ -5,12 +5,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import dev.fitko.fitconnect.client.SubscriberClient;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 class SecurityTxtIntegrationTest {
 
   @Autowired private MockMvc mockMvc;
+  @MockBean SubscriberClient subscriberClient;
 
   @Test
   void shouldExposeSecurityTxt() throws Exception {
