a277d4216651a125b9202e1715cd90d5f6b8a1a4
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-01-15 09:36:55 +0100

    only log egvp client exceptions in callback controller

--- src/main/java/de/bund/digitalservice/a2j/controller/SubscriberController.java
+++ src/main/java/de/bund/digitalservice/a2j/controller/SubscriberController.java
@@ -4,6 +4,8 @@ import de.bund.digitalservice.a2j.service.egvp.client.EgvpClientException;
 import de.bund.digitalservice.a2j.service.subscriber.SubscriberService;
 import dev.fitko.fitconnect.api.domain.model.callback.NewSubmissionsCallback;
 import dev.fitko.fitconnect.api.domain.model.submission.SubmissionForPickup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +19,15 @@ public class SubscriberController {
     this.service = service;
   }
 
+  private static final Logger logger = LoggerFactory.getLogger(SubscriberController.class);
+
   @PostMapping("callbacks/fit-connect")
   public void newSubmission(@RequestBody NewSubmissionsCallback callback) {
     for (SubmissionForPickup submissionForPickup : callback.getSubmissions()) {
       try {
         service.pickUpSubmission(submissionForPickup);
       } catch (EgvpClientException e) {
-        throw new RuntimeException(e.getMessage());
+        logger.error("unable to propagate submission {}", submissionForPickup.getSubmissionId(), e);
       }
     }
   }
